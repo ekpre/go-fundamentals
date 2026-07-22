f307564240b45f362c1cd71a78c288b610fec0df
aerial <aerial@disroot.org>
2026-07-22 09:03:22 +0100

    04

---
+++ upper-string.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+
+func main() {
+    r := bufio.NewReader(os.Stdin)
+    line, _ := r.ReadString('\n')
+    line = strings.TrimRight(line, "\r\n")
+		upperLine := strings.ToUpper(line)
+		fmt.Println(upperLine)
+    _ = fmt.Sprint
+}
+
